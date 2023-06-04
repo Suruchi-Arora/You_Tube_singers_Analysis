6d72880379b24617f61613ac04c47b2270ce9553
Suruchi <111527817+Suruchi-Arora@users.noreply.github.com>
2023-06-05 01:34:02 +0530

    Update deployment.py  apikey

--- deployment.py
+++ deployment.py
@@ -6,11 +6,7 @@ import seaborn as sn
 import time
 from matplotlib import pyplot as plt
 import plotly.express as px
-# import configparser
-# config = configparser.ConfigParser()
-# config.read('config.ini')
-# api_key = config['DEFAULT']['API_KEY']
-api_key='AIzaSyBWGf8pmToHEWpzZoxLwWsMNRLIK4QLJn0'
+from config import api_key
 
 channel_ids=['UCn3YGmLvUXFkHIT9_z4mRTQ',    #Neeti Mohan
 'UCZRdNleCgW-BGUJf-bbjzQg',                 #Diljit Dosanjh
